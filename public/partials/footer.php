c7cd45a832ab41ccdd741e209916b442c761db49
a9sk <920a9sk42f76c765@proton.me>
2025-02-09 18:50:55 +0100

    feat: added style and footer to be included

---
+++ public/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <p>© 2025 a9sk. All rights reserved.</p>
+    <p>
+        <a href="privacy_policy.php" class="text-light">Privacy Policy</a> |
+        <a href="terms.php" class="text-light">Terms of Service</a> |
+        <a href="https://github.com/a9sk/whisper" class="text-light">Source code</a>
+    </p>
+</footer>
